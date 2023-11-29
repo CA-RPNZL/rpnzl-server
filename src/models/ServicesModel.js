478e114e02487f53e14dbd8ff68f614f04b7ac6f
Helen T <helen-thai@hotmail.com>
2023-11-29 22:16:38 +1100

    fix: updated seed.js ServicesModel descriptions and save

--- src/models/ServicesModel.js
+++ src/models/ServicesModel.js
@@ -1,5 +1,5 @@
 // Import Mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ServicesSchema = new Schema({
@@ -21,11 +21,13 @@ const ServicesSchema = new Schema({
     duration: {
         type: Number,
         required: true,
-        unique: false}
+        unique: false
+    }
 });
 
+
 // Create Services document
-const Services = mongoose.model('Services', ServicesSchema);
+const Services = mongoose.model("Services", ServicesSchema);
 
 module.exports = {
     Services
--- src/seed.js
+++ src/seed.js
@@ -3,6 +3,12 @@ const mongoose = require("mongoose");
 const { dbConnect } = require("./database");
 const { Services } = require("./models/ServicesModel");
 
+
+// Import and configure dotenv
+require("dotenv").config();
+
+
+// Connect to the database
 dbConnect().then(async ()=> {
     console.log("Creating seed data");
 
@@ -11,37 +17,42 @@ dbConnect().then(async ()=> {
     let cut = new Services({
         name: "Cut",
         price: "$40+",
-        description: "Includes: Hair wash, blow dry and cut\\nPrices and time will vary due to thickness and length of hair.",
+        description: "Includes: Hair wash, blow dry and cut. Prices and time will vary due to thickness and length of hair.",
         duration: "30"
     });
+    await cut.save();
 
     let perm = new Services({
         name: "Perm",
         price: "$250 - $500",
-        description: "Includes: Perm, wash and styling\\nPrices and time will vary due to thickness and length of hair.",
+        description: "Includes: Perm, wash and styling. Prices and time will vary due to thickness and length of hair.",
         duration: "120"
     });
+    await perm.save();
 
     let colour = new Services({
         name: "Colour",
         price: "$270+",
-        description: "Includes: Colouring, hair wash and styling\\nPrices and time will vary due to thickness and length of hair.",
+        description: "Includes: Colouring, hair wash and styling. Prices and time will vary due to thickness and length of hair.",
         duration: "120"
     });
+    await colour.save();
 
     let cutAndColour = new Services({
         name: "Cut & colour",
         price: "$280 - $530",
-        description: "Includes: Hair wash, colour, cut and styling\\nPrices and time will vary due to thickness and length of hair.",
+        description: "Includes: Hair wash, colour, cut and styling. Prices and time will vary due to thickness and length of hair.",
         duration: "150"
     });
+    await cutAndColour.save();
 
     let consultation = new Services({
         name: "Consultation",
         price: "Free!",
-        description: "Free consultation with each appointment.\\nWe will discuss final prices and approximate length of appointment.\\nProvide full information on what will be completed throughout the appointment.",
+        description: "Free consultation with each appointment. We will discuss final prices and approximate length of appointment.\\nProvide full information on what will be completed throughout the appointment.",
         duration: "15"
     });
+    await consultation.save();
 
 
 });
