e6191a16c51050267bce683b954ab96809e3a5fc
StevieG46 <126635670+StevieG46@users.noreply.github.com>
2023-11-29 22:41:52 +1100

    feat: User Module started

---
+++ src/models/UserModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        unique: false,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        unique: false,
+    },
+    mobileNumber: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: false,
+    },
+    is_admin: {
+        type: Boolean,
+        default: false
+    },
+
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {
+    User
+}
