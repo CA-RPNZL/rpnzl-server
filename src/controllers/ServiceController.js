796a71657e9631806639296ce9e97e9d3a660b6a
Helen T <helen-thai@hotmail.com>
2023-12-02 01:02:31 +1100

    feat: post, patch and delete services route complete

--- src/controllers/ServiceController.js
+++ src/controllers/ServiceController.js
@@ -21,19 +21,43 @@ router.get("/", async (request, response) => {
 
 // Show service by service id
 // Doesn't need authentication
+// GET /services/id/:id
 router.get("/id/:id", async (request, response) => {
-    let result = await Service.findById(request.params.id)
+    let result = await Service.findById(request.params.id);
 
     response.json(result);
-})
+});
 
 // Create a new service
 // Need admin authentication
+// POST /services
+router.post("/", async (request, response) => {
+    let newService = await Service.create(request.body).catch(error => error);
 
-// Update an existing service
-// Need admin authentication
+    response.json(newService);
+});
 
-// Delete a service
+// Update an existing service by id
 // Need admin authentication
+// PATCH /services/id:id
+router.patch("/id/:id", async (request, response) => {
+    // Show updated service
+    let result = await Service.findByIdAndUpdate(request.params.id, request.body, { returnDocument: "after" }).catch(error => error);
+
+    response.json({
+        updatedService: result
+    });
+});
+
+// Delete a service by id
+// Need admin authentication
+// DELETE /services/id/:id
+router.delete("/id/:id", async (request, response) => {
+    let result = await Service.findByIdAndDelete(request.params.id);
+
+    response.json({
+        deletedService: result
+    });
+});
 
 module.exports = router;
