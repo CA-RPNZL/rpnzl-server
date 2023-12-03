2c9eca7725d07bc7ffbf422422cd54be35ce7fb6
Helen T <helen-thai@hotmail.com>
2023-12-03 18:43:24 +1100

    feat: created validateJwt function and authAsAdmin function

--- src/functions/auth_function.js
+++ src/functions/auth_function.js
@@ -14,11 +14,10 @@ async function comparePassword(reqPassword, userPassword) {
 
     // bcrypt.compare(myPlaintextPassword, hashedPassword)
     isPasswordCorrect = await bcrypt.compare(reqPassword, userPassword);
-    console.log(reqPassword);
-    console.log(userPassword);
-    console.log(isPasswordCorrect);
+
     return isPasswordCorrect;
 };
+
 
 // Generate JWT
 function generateJwt(userId, isAdmin, isHairstylist) {
@@ -34,15 +33,56 @@ function generateJwt(userId, isAdmin, isHairstylist) {
     );
 };
 
+
 // Validate JWT
-// function validateJwt = (request, response, next) => {
-//     let suppliedToken = request.headers.jwtl;
-//     // jwt.verify(token, secretorPublicKey, options)
-//     jwt.verify(suppliedToken, process.env.JWT_SECRETKEY, )
-// }
+function validateJwt(request, response, next) {
+    try {
+        // Grab the JWT from the header
+        let suppliedToken = request.headers.jwt;
+    
+        // Handle if no token found
+        if (!suppliedToken) {
+            return response.status(401).json({
+                error: "Unauthorised - missing token."
+            });
+        };
+        
+        // Verify if the JWT is valid
+        // jwt.verify(token, secretorPublicKey, options)
+        const decodedToken = jwt.verify(suppliedToken, process.env.JWT_SECRETKEY);
+    
+        // Set the decoded token as the request.user
+        request.user = decodedToken;
+    
+        // Continue to next middleware
+        next();
+    } catch (error) {
+        // Handle error if issue with token
+        return response.status(401).json({
+            error: "Unauthorised - invalid token."
+        });
+    };
+};
+
+
+// Authorise as admin
+function authAsAdmin(request, response, next) {
+    // Check if the user is admin
+    if (request.user.is_admin) {
+        // If user is admin, continue to next middleware
+        next();
+    } else {
+        // Handle error if user is not admin
+        return response.status(403).json({
+            error: "You do not have authorisation to proceed."
+        });
+    };
+};
 
 
 module.exports = {
     comparePassword,
-    generateJwt
+    generateJwt,
+    validateJwt,
+    authAsAdmin
 };
