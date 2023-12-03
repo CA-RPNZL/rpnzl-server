ed922f6ba6ad7b0b73852bd0f1b919a9e83941db
Helen T <helen-thai@hotmail.com>
2023-12-03 19:03:44 +1100

    feat: updated error handling, added auth middleware

--- src/controllers/ServiceController.js
+++ src/controllers/ServiceController.js
@@ -1,46 +1,63 @@
 // Import Express JS
 const express = require("express");
 
+// Create an instance of an Express Router
+const router = express.Router();
+
 // Import Mongoose
 const mongoose = require("mongoose");
 
 const { Service } = require("../models/ServiceModel");
-
-// Create an instance of an Express Router
-const router = express.Router();
+const { validateJwt } = require("../functions/authentication");
+const { authAsAdmin } = require("../functions/authorisation");
 
 
 // Show all services
 // Doesn't need authentication
 // GET /services
 router.get("/", async (request, response) => {
-  let result = await Service.find({});
-
-  response.json(result);
+    try {
+        let result = await Service.find({});
+        response.json(result);
+    } catch (error) {
+        response.status(500).json({
+            error: error
+        })
+    };
 });
 
 // Show service by service id
 // Doesn't need authentication
 // GET /services/id/:id
 router.get("/id/:id", async (request, response) => {
-    let result = await Service.findById(request.params.id);
-
-    response.json(result);
+    try {
+        let result = await Service.findById(request.params.id);
+        response.json(result);
+    } catch (error) {
+        response.status(500).json({
+            error: error
+        })
+    };
 });
 
 // Create a new service
 // Need admin authentication
 // POST /services
-router.post("/", async (request, response) => {
-    let newService = await Service.create(request.body).catch(error => error);
-
-    response.json(newService);
+router.post("/", validateJwt, authAsAdmin, async (request, response) => {
+    try {
+        let newService = await Service.create(request.body);
+        response.json(newService);
+    } catch (error) {
+        response.status(500).json({
+            error: error
+        })
+    };
 });
 
 // Update an existing service by id
 // Need admin authentication
 // PATCH /services/id:id
-router.patch("/id/:id", async (request, response) => {
+router.patch("/id/:id", validateJwt, authAsAdmin, async (request, response) => {
     // Show updated service
     let result = await Service.findByIdAndUpdate(request.params.id, request.body, { returnDocument: "after" }).catch(error => error);
 
@@ -52,7 +69,7 @@ router.patch("/id/:id", async (request, response) => {
 // Delete a service by id
 // Need admin authentication
 // DELETE /services/id/:id
-router.delete("/id/:id", async (request, response) => {
+router.delete("/id/:id", validateJwt, authAsAdmin, async (request, response) => {
     let result = await Service.findByIdAndDelete(request.params.id);
 
     response.json({
