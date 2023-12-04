2c9e304afd5516a121594535900869a71eb02948
StevieG46 <126635670+StevieG46@users.noreply.github.com>
2023-12-04 20:35:40 +1100

    fix: Phone number to string and ._id in seed file

--- src/seed.js
+++ src/seed.js
@@ -9,7 +9,7 @@ dbConnect().then(async ()=> {
     let newAdmin = new User({
         firstName: 'George',
         lastName: 'Sheridan',
-        mobileNumber: 0o411222333,
+        mobileNumber: '0411222333',
         email: 'admin@mail.com',
         password: 'adminpassword',
         is_admin: true,
@@ -17,19 +17,23 @@ dbConnect().then(async ()=> {
     let newHairstylist = new User({
         firstName: 'Michelle',
         lastName: 'Smith',
-        mobileNumber: 0o433444555,
+        mobileNumber: '0433444555',
         email: 'michelle@mail.com',
         password: 'hairstylistpassword',
         is_hairstylist: true,
         // Uses ID value of services eg. [cutServiceID, consultationServiceId]
-        services: [0, 4],
+        services: [cut._id.toString(), consultation._id.toString()],
     });
     let newUser = new User({
         firstName: 'Bianca',
         lastName: 'Lopez',
-        mobileNumber: 0o477444556,
+        mobileNumber: '0477444556',
         email: 'bianca@mail.com',
         password: 'clientpassword',
+    });
+
+    await User.create([newAdmin, newHairstylist, newUser]).catch(error => {
+        console.log("An error occured:\n" + error)
     });
 });
 
