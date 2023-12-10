ad1e21be0f973e6b1f528dab6f98fe1f09eed23a
StevieG46 <126635670+StevieG46@users.noreply.github.com>
2023-12-10 20:13:57 +1100

    fix: Remove middleware to allow for testing

--- src/controllers/AppointmentController.js
+++ src/controllers/AppointmentController.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { Appointment } = require("../models/AppointmentModel");
-const { authAsAdminOrUser, authAsAdmin, authAsHairstylist } = require("../functions/authorisation");
-const { validateJwt } = require("../functions/authentication");
+// const { authAsAdminOrUser, authAsAdmin, authAsHairstylist } = require("../functions/authorisation");
+// const { validateJwt } = require("../functions/authentication");
 
 // Get all appointments
 // GET /appointments
-router.get("/", validateJwt, authAsAdmin, async (request, response) => {
+router.get("/", async (request, response) => {
     try {
       const result = await Appointment.find({});
       response.json(result);
@@ -17,7 +17,7 @@ router.get("/", validateJwt, authAsAdmin, async (request, response) => {
   
 // Get appointment by ID
 // GET /appointments/id/:id
-router.get("/id/:id", validateJwt, authAsAdminOrUser, async (request, response) => {
+router.get("/id/:id", async (request, response) => {
   try {
     const result = await Appointment.findById(request.params.id);
     if (!result) {
@@ -33,7 +33,7 @@ router.get("/id/:id", validateJwt, authAsAdminOrUser, async (request, response) 
 // Get appointments by hairstylist ID
 // Need Hairstylist authentication
 // GET /appointments/hairstylist/:hairstylistId
-router.get("/hairstylist/:hairstylistId", validateJwt, authAsHairstylist, async (request, response) => {
+router.get("/hairstylist/:hairstylistId", async (request, response) => {
   try {
     const hairstylistId = request.params.hairstylistId;
     const appointments = await Appointment.find({ hairstylist: hairstylistId });
@@ -47,7 +47,7 @@ router.get("/hairstylist/:hairstylistId", validateJwt, authAsHairstylist, async 
 // Create a new appointment
 // Need user or admin authentication
 // POST /appointments
-router.post("/", validateJwt, authAsAdminOrUser, async (request, response) => {
+router.post("/", async (request, response) => {
   try {
       let newAppointment = await Appointment.create(request.body);
       response.status(201).json(newAppointment);
@@ -60,7 +60,7 @@ router.post("/", validateJwt, authAsAdminOrUser, async (request, response) => {
 // Update an existing appointment by ID
 // Need user or admin authentication
 // PATCH /appointments/id/:id
-router.patch("/id/:id", validateJwt, authAsAdminOrUser,  async (request, response) => {
+router.patch("/id/:id", async (request, response) => {
   // Show updated service
   let result = await Appointment.findByIdAndUpdate(request.params.id, request.body, { returnDocument: "after" }).catch(error => error);
 
@@ -72,7 +72,7 @@ router.patch("/id/:id", validateJwt, authAsAdminOrUser,  async (request, respons
 // Delete appointment by id
 // Need user or admin authentication
 // DELETE /appointments/id/:id
-router.delete("/id/:id", validateJwt, authAsAdminOrUser, async (request, response) => {
+router.delete("/id/:id", async (request, response) => {
   let result = await Appointment.findByIdAndDelete(request.params.id);
 
   response.json({
