593962a361bf7b81337629056b59b70a359a6f20
Helen T <helen-thai@hotmail.com>
2023-12-17 01:54:28 +1100

    feat: add authAsAdmin to ServiceController

--- src/functions/authorisation.js
+++ src/functions/authorisation.js
@@ -3,6 +3,7 @@ function authAsAdmin(request, response, next) {
     // Check if the user is admin
     if (request.user.is_admin) {
         // If user is admin, continue to next middleware
+        console.log("Authorised as admin");
         next();
     } else {
         // Handle error if user is not admin
@@ -17,6 +18,7 @@ function authAsHairstylist(request, response, next) {
     // Check if the user is a hairstylist
     if (request.user.is_hairstylist) {
         // If user is hairstylist, continue to next middleware
+        console.log("Authorised as hairstylist");
         next();
     } else {
         // Handle error if user is not hairstylist
@@ -33,6 +35,7 @@ function authAsUser(request, response, next) {
     const targetUser = request.params.id;
     // Check  if the logged in user is the user
     if (loggedInUser === targetUser) {
+        console.log("Authorised as logged in user");
         next();
     } else {
         // Handle error if logged in user is not the user
@@ -49,6 +52,7 @@ function authAsAdminOrUser(request, response, next) {
     const targetUser = request.params.id;
     // Check  if the logged in user is the user
     if (request.user.is_admin || loggedInUser === targetUser) {
+        console.log("Authorised as admin, or logged in user");
         next();
     } else {
         // Handle error if logged in user is not the user
