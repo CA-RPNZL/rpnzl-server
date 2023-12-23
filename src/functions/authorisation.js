2b70d77a928e8717db8fd44960e65e4cfdc35dd6
Helen T <helen-thai@hotmail.com>
2023-12-23 13:47:28 +1100

    fix: removed authAsHairstylist as unused

--- src/functions/authorisation.js
+++ src/functions/authorisation.js
@@ -12,21 +12,6 @@ function authAsAdmin(request, response, next) {
     };
 };
 
-// Authorise as hairstylist --- not used
-// function authAsHairstylist(request, response, next) {
-//     // Check if the user is a hairstylist
-//     if (request.user.is_hairstylist) {
-//         // If user is hairstylist, continue to next middleware
-//         console.log("Authorised as hairstylist");
-//         next();
-//     } else {
-//         // Handle error if user is not hairstylist
-//         return response.status(403).json({
-//             error: "You do not have authorisation to proceed."
-//         });
-//     };
-// }
-
 // Authorise as logged in user is target user
 // uses params: userId
 function authAsUser(request, response, next) {
@@ -64,7 +49,6 @@ function authAsAdminOrUser(request, response, next) {
 
 module.exports = {
     authAsAdmin,
-    // authAsHairstylist,
     authAsUser,
     authAsAdminOrUser
 };
