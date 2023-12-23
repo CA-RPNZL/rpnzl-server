dfc716c77c67f48dacca93b2df8d719c153edd80
Helen T <helen-thai@hotmail.com>
2023-12-23 13:17:22 +1100

    feat: added server test for 404 route

--- testing/server.test.js
+++ testing/server.test.js
@@ -10,3 +10,11 @@ describe("GET /", () => {
         expect(response.text).toBe("Hello world!");
     });
 });
+
+describe("GET /", () => {
+    it("should return 'Hello world!'", async () => {
+        const response = await supertest(app).get("/404");
+        expect(response.statusCode).toBe(404);
+        expect(response.text).toContain("This path could not be found!");
+    });
+});
